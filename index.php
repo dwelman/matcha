b78e878aa566bf804dde3f827b18a9873241bf1f
David WELMAN <daviwel@e5r7p1.jnb.42.fr>
2016-10-25 10:36:35 +0200

    added bootstrap and index

---
+++ index.php
@@ -0,0 +1,14 @@
+<!doctype HTML>
+<html>
+	<head>
+		<meta charset="utf-8">
+		<meta name="viewport" content="width=device-width, initial-scale=1.0">
+		<title>Matcha</title>
+		<link href="bootstrap/css/bootstrap.min.css" rel="stylesheet">
+	</head>
+	<body>
+
+		<script src="bootstrap/js/jquery.min.js"></script>
+		<script src="bootstrap/js/bootstrap.min.js"></script>
+	</body>
+</html>
