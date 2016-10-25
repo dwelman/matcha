c9f8d38aabe44b16f85c99c551f5ccb5e603b86b
David WELMAN <daviwel@e5r7p1.jnb.42.fr>
2016-10-25 11:57:30 +0200

    made some changes

--- index.php
+++ index.php
@@ -4,10 +4,10 @@
 		<title>Matcha</title>
 		<meta charset="utf-8">
 		<meta name="viewport" content="width=device-width, initial-scale=1.0">
-		<link href="bootstrap/css/bootstrap.min.css" rel="stylesheet">
+		<link href="css/bootstrap.min.css" rel="stylesheet">
 		<link href="css/style.css" rel="stylesheet">
-		<script src="bootstrap/js/jquery.min.js"></script>
-		<script src="bootstrap/js/bootstrap.min.js"></script>
+		<script src="js/jquery.min.js"></script>
+		<script src="js/bootstrap.min.js"></script>
 	</head>
 	<body>
 		<div class="site-wrapper">
