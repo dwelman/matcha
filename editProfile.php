a0c847eae6eed5ced61b4649ec13c780f42ec0a4
David WELMAN <daviwel@e5r7p1.jnb.42.fr>
2016-11-03 12:31:40 +0200

    a

--- editProfile.php
+++ editProfile.php
@@ -37,6 +37,12 @@
           </ul>
           <ul class="nav navbar-nav navbar-right">
 		  	<li class="dropdown">
+                <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">Notifications <span class="caret"></span></a>
+                <ul class="dropdown-menu">
+				</ul>
+              </li>
+
+		  	<li class="dropdown">
                 <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">History <span class="caret"></span></a>
                 <ul class="dropdown-menu">
 				<?php
